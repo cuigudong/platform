9ea9c4388a61f82fb63de9219890937997d618aa
gd.cui <136253588@qq.com>
2017-12-29 15:36:49 +0800

    增加测试

---
+++ src/test/java/com/core/utils/BeanMapperTest.java
@@ -0,0 +1,19 @@
+package com.core.utils;
+
+import junit.framework.TestCase;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sks on 2017/12/29.
+ */
+public class BeanMapperTest extends TestCase {
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+}
